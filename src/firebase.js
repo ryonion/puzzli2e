f8d811842c75e91541ce142ad8f078405c87a89c
ryanzhang_kodee <ryan@Kodee.net>
2022-06-22 23:25:23 -0700

    initial

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAOvejZqOTwFwWyQhkve44cKI1w--aN2Vg",
+    authDomain: "puzzli2e.firebaseapp.com",
+    projectId: "puzzli2e",
+    storageBucket: "puzzli2e.appspot.com",
+    messagingSenderId: "410773759743",
+    appId: "1:410773759743:web:3b3f600440fa3e715dceca"
+};
+
+export default firebaseConfig;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Puzzle from "./components/puzzle";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Puzzle />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
